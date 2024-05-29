c32d72130287937016d4267341302e775db729e1
Andrew Song <73975626+Minseok0310@users.noreply.github.com>
2024-05-29 13:47:50 -0400

    Check stock statuses of all models

--- main.py
+++ main.py
@@ -1,11 +1,36 @@
+# Import and Setup Drivers
 from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-driver = webdriver.Chrome(ChromeDriverManager().install())
+driver = webdriver.Chrome()
+wait = WebDriverWait(driver, 10)
 
+# Navigate to Steam Store page
 driver.maximize_window()
-driver.get("https://store.steampowered.com/")
+driver.get("https://store.steampowered.com/sale/steamdeckrefurbished/")
+
+assert "steam" in driver.current_url
+
 print("Application title is ", driver.title)
-print("Application url is ", driver.current_url)
+print("Application url is", driver.current_url)
+
+# Check all three stock statuses (64, 256 & 512gb)
+elements = wait.until(EC.presence_of_all_elements_located((By.CLASS_NAME, "I_6PnNNc2kMM38zamMgjd.CartBtn")))
+
+if len(elements) == 3:
+    models = ["64GB", "256GB", "512GB"]
+    stockStatus = []
+
+    for i in range(0,len(elements)):
+        if "Out of stock" in elements[i].text:
+            stockStatus.append("Out of Stock")
+        else:
+            stockStatus.append("In Stock")
+    
+    for i in range(0,len(elements)):
+        print(models[i], " is ", stockStatus[i])
 
 driver.quit()
