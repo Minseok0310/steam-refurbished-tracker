205308b1fc74262fb8876a509cfc5a63ed9e2dd4
Andrew Song <73975626+Minseok0310@users.noreply.github.com>
2024-05-29 11:28:40 -0400

    Setting up and initializing selenium/chrome drivers

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello")
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+driver = webdriver.Chrome(ChromeDriverManager().install())
+
+driver.maximize_window()
+driver.get("https://store.steampowered.com/")
+print("Application title is ", driver.title)
+print("Application url is ", driver.current_url)
+
+driver.quit()
